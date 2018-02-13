f882dc5fa729f4b3b4ad184b28ff2010737834dd
Alexander Kakushin <alexandrkakushin@yandex.ru>
2018-02-13 14:15:56 +0300

    Расширение возможностей компоненты 1. Полное соответствие 2. Соответствие в любой части строки 3. Соответствие в начале строки 4. Замена во всей строке 5. Замена первого

--- src/main/java/ru/ak/regex/RegexClient.java
+++ src/main/java/ru/ak/regex/RegexClient.java
@@ -3,7 +3,7 @@ package ru.ak.regex;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import java.util.regex.*;
+import java.util.regex.Pattern;
 
 /**
  * Web-сервис для реализация интерфейса взаимодействия
@@ -13,11 +13,49 @@ import java.util.regex.*;
 public class RegexClient {
 
     @WebMethod
-    public boolean check(
+    public boolean matches(
             @WebParam(name = "pattern") String pattern,
             @WebParam(name = "text") String text) {
 
         Pattern ptr = Pattern.compile(pattern);
         return ptr.matcher(text).matches();
+    }
+
+    @WebMethod
+    public boolean lookingAt(
+            @WebParam(name = "pattern") String pattern,
+            @WebParam(name = "text") String text) {
+
+        Pattern ptr = Pattern.compile(pattern);
+        return ptr.matcher(text).lookingAt();
+    }
+
+    @WebMethod
+    public boolean find(
+            @WebParam(name = "pattern") String pattern,
+            @WebParam(name = "text") String text) {
+
+        Pattern ptr = Pattern.compile(pattern);
+        return ptr.matcher(text).find();
+    }
+
+    @WebMethod
+    public String replaceFirst(
+            @WebParam(name = "pattern") String pattern,
+            @WebParam(name = "text") String text,
+            @WebParam(name = "replacement") String replacement) {
+
+        Pattern ptr = Pattern.compile(pattern);
+        return ptr.matcher(text).replaceFirst(replacement);
+    }
+
+    @WebMethod
+    public String replaceAll(
+            @WebParam(name = "pattern") String pattern,
+            @WebParam(name = "text") String text,
+            @WebParam(name = "replacement") String replacement) {
+
+        Pattern ptr = Pattern.compile(pattern);
+        return ptr.matcher(text).replaceAll(replacement);
     }
 }
