885c82196b888ab1674b8eaa40b98f533b5a518a
Alexander Kakushin <alexandrkakushin@yandex.ru>
2019-08-31 00:19:38 +0300

    Адаптация для 1С

--- src/main/java/ru/ak/info/package-info.java
+++ src/main/java/ru/ak/info/package-info.java
@@ -5,7 +5,7 @@
                 @XmlNs(namespaceURI = "http://regex.ak.ru/", prefix = ""),
                 @XmlNs(namespaceURI = "http://www.w3.org/2001/XMLSchema-instance", prefix = "xsi"),
                 @XmlNs(namespaceURI = "http://www.w3.org/2001/XMLSchema", prefix = "xs")})
-package ru.ak.model;
+package ru.ak.info;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
